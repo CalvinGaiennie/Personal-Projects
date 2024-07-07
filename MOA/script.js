3e3053a0360903a79b4142bb0e48f86e5124dcac
CalvinGaiennie <calvingaiennie@gmail.com>
2024-07-07 01:55:13 -0500

    I created an app mostly on my own
    I made a program to help zero in optics. I had to do some googling on the html. After hours of trying to get the Javascript right I asked chat gbt to write the program and based my javascript off of its solution. It's solution was much simpler than mine and actually worked. It's solution avoided the problem of scope very simply and easily. I wrote down all of the different calculations I had been trying to do in seperate functions into one formula and gave that to chat. If I had done that in the first place I may have come to a similar solution to chat and it probably wouldve worked.

--- MOA/script.js
+++ MOA/script.js
@@ -1,87 +1,39 @@
+"use strict";
 // https://lprgc.org/wp-content/uploads/2011/12/Sight-Adjustments-using-Minutes-Of-Angle.pdf
 
-/*
-variables needed:
-distance from target
-    shooterDistance
-moa per click on your turret
-    MOAPerClick
-vertical distance from center
-    elevationD
-horizontal distance from center
-    windageD
-i will either need to make certain directions negative or implement right and left and up and down some other way.
-*/
+function calculate() {
+  // Get input values
 
-//A minute of angle, usually used as a measure of group size, target size or shot dispersion, spans 1.047 inch at 100 yards but we round down to 1”. To calculate MOA at any distance, multiply 1.047 by the distance in yards and divide by 100.
-// formula
-// 1 moa is 1.047 inches at 100 yards
-// multiply 1.047 by the distance in yards and divide by 100. this tells you how big 1 moa is
+  const shooterDistance = parseFloat(
+    document.getElementById("shooterDistance").value
+  );
 
-// // );
-// const distance = Number(document.querySelector(".distance").value);
-// const MOAFactor = Number(document.querySelector(".MOAFactor").value);
-// const windage = Number(document.querySelector(".windage").value);
-// const elevation = Number(document.querySelector(".elevation").value);
+  const MOAFactor = parseFloat(document.getElementById("MOAFactor").value);
 
-const solution = Number(document.querySelector(".generate").value);
+  const windage = parseFloat(document.getElementById("Windage").value);
 
-///////////////////////////////////////////////////////////////////////
-//When people enter their info these functions run and create variables
-document.querySelector(".OK1").addEventListener("click", function () {
-  const distance = Number(document.querySelector(".distance").value);
-  console.log(distance);
-});
+  const elevation = parseFloat(document.getElementById("Elevation").value);
 
-document.querySelector(".OK2").addEventListener("click", function () {
-  const MOAFactor = Number(document.querySelector(".MOAFactor").value);
-  console.log(MOAFactor);
-});
+  // Check for valid inputs
+  if (
+    isNaN(shooterDistance) ||
+    isNaN(MOAFactor) ||
+    isNaN(windage) ||
+    isNaN(windage)
+  ) {
+    alert("Please enter valid numbers for all inputs.");
+    return;
+  }
 
-document.querySelector(".OK3").addEventListener("click", function () {
-  const windage = Number(document.querySelector(".windage").value);
-  console.log(windage);
-});
+  // Perform the calculation using the given formula
+  const w = Math.round(windage / (shooterDistance * (1.047 / 100)) / MOAFactor);
 
-document.querySelector(".OK4").addEventListener("click", function () {
-  const elevation = Number(document.querySelector(".elevation").value);
-  console.log(elevation);
-});
-//////////////////////////////////////////////////////////////////////////
-//math formula 
-// MOASize = distance * 1.047 / 100
-//# of MOA needed to get on target (MOANum) = windage(or elevation)(in inches) / MOASize
-// number of clicks needed to get on target numOfClicks= MOANum /MOAF (MOA factor of the turret on your optic) 
+  const e = Math.round(
+    elevation / (shooterDistance * (1.047 / 100)) / MOAFactor
+  );
 
-////////////////////////////////////////////////////////////////////////////////////////////////
-//when someone clicks generate solution
-
-
-//I tried nesting the formulas but it doesnt seem to work. I didnt expect it to but i tried.
-document.querySelector(".generate").addEventListener("click", function () {
-  const numOfClicksEf = function (MOAFactor, elevation, distance) {
-    const MOANEFuncf = function (elevation) {
-      const MOASizef = function (distance) {
-        const MOASizef = (distance * 1.047) / 100;
-        return MOASize;
-      };
-      const MOANE = elevation / MOASize;
-      return MOANE;
-    };
-    const numOfClicksE = MOANE / MOAFactor;
-    return numOfClicksE;
-  };
-  console.log(numOfClicksE);
- 
-
-
-  //When someone clicks generate solution
-
-// document.querySelector(".generate").addEventListener("click", function () {
-// calculateE(distance, MOAFactor, elevation);
-// calculateW(distance, MOAFactor, windage);
-// displayMessage(
-//     "Adjust your optic ${blank} clicks ${blank} and ${blank} clicks ${blank};
-//   );
-// });
-*/
+  //display the result
+  document.getElementById(
+    "solutionText"
+  ).textContent = `Move right ${w} clicks and up ${e} clicks.`;
+}
