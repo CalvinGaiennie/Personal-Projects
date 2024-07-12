81200a52d4723be76b6e5e338ebc72b04704a7ec
CalvinGaiennie <calvingaiennie@gmail.com>
2024-07-11 22:58:36 -0500

    More playing around with projects trying to get better at having javascript and HTML interact with each other

---
+++ Evan/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function calculate() {
+  const A = parseFloat(document.getElementById("initialInvestment").value);
+
+  document.getElementById("solutionText").textContent = `${A}`;
+}
--- Joke/script.js
+++ Joke/script.js
@@ -1,23 +1,33 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(typeof A);
+function calculate() {
+  const A = parseFloat(document.getElementById('answer').value);
+  if (A === 'Yes')
+    document.getElementById('solutionText').textContent = 'You better beee!';
+  else document.getElementById('solutionText').textContent = 'You better be!';
+}
 
-const openModal = function () {
-  console.log('Button clicked');
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
+///////////////////////////////////////
+//Modal Shit
+// const modal = document.querySelector('.modal');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelector('.close-modal');
+// const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
+// const openModal = function () {
+//   console.log('Button clicked');
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// const closeModal = function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// };
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+// btnCloseModal.addEventListener('click', closeModal);
+// overlay.addEventListener('click', closeModal);
