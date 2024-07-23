44a8e95eedb9caa8606cdcd7b7dd6dac2704c892
CalvinGaiennie <calvingaiennie@gmail.com>
2024-07-23 03:19:53 -0500

    Added a Waste Array
    I added an array that accounts for waste in the stringers. I would later like to have the function calculate the materials needed by the length of each individual side of the fence. Better calculating waste on stringers as well as not overestimating the number of boards and screws needed. At that point I may be able to have the function test and see whether or not the extra needed for one short section could be taken from the waste on another section so that instead of waste stacking up on multiple short sections waste from one section would be used on another not only taking it out of the waste array but also preventing more waste from being created.

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -1,8 +1,7 @@
 function calculate() {
   // Get input values
-
+  const wasteArray = new Array(16).fill(0);
   const distance = parseFloat(document.getElementById("distance").value);
-
   const boardNum = parseFloat(document.getElementById("boardNum").value);
   //   const sidesNum = parseFloat(document.getElementById("sides").value);
   // Check for valid inputs
@@ -17,6 +16,8 @@ function calculate() {
   }
   if (distance / 16 - Math.trunc(distance / 16) > 0) {
     numOfSt = Math.trunc(distance / 16) * 3 + 3;
+    waste = 15 - (distance / 16 - Math.trunc(distance / 16)) * 16;
+    wasteArray[waste] += 3;
   } else {
     numOfSt = (distance / 16) * 3;
   }
@@ -31,4 +32,6 @@ function calculate() {
   document.getElementById(
     "solutionText"
   ).textContent = `You need ${numOfP} posts, ${numOfC} bags of concrete, ${numOfSt} 2x4s, ${numOfB} boards, ${numOfSSc} short screws and, ${numOfLSc} long screws.`;
+  console.log(waste);
+  console.log(wasteArray);
 }
