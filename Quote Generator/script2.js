4c3973c7c89f90c449126338a6c066cacc4b3b3c
CalvinGaiennie <calvingaiennie@gmail.com>
2024-07-28 23:43:41 -0500

    Fixing Bug

--- Quote Generator/script2.js
+++ Quote Generator/script2.js
@@ -1,8 +1,7 @@
 // "use strict";
 function calculate() {
-  // Get input values
-  const wasteArray = new Array(16).fill(0);
   const distance = parseFloat(document.getElementById("distance").value);
+
   const boardNum = parseFloat(document.getElementById("boardNum").value);
   //   const sidesNum = parseFloat(document.getElementById("sides").value);
   // Check for valid inputs
@@ -17,8 +16,6 @@ function calculate() {
   }
   if (distance / 16 - Math.trunc(distance / 16) > 0) {
     numOfSt = Math.trunc(distance / 16) * 3 + 3;
-    waste = 15 - (distance / 16 - Math.trunc(distance / 16)) * 16;
-    wasteArray[waste] += 3;
   } else {
     numOfSt = (distance / 16) * 3;
   }
@@ -26,18 +23,11 @@ function calculate() {
   console.log(numOfSt);
   numOfP = numOfUnits + 1;
   numOfC = numOfP;
-  numOfUnitsB = numOfUnits - 1 + (distance - (numOfUnits - 1) * 8) / 8;
-  console.log(numOfUnitsB);
-  numOfB = Math.ceil(numOfUnitsB * boardNum);
-  console.log(numOfB);
+  numOfB = numOfUnits * boardNum;
   numOfSSc = numOfB * 6;
   numOfLSc = numOfSt * 6;
-
   //display the result
   document.getElementById(
     "solutionText"
   ).textContent = `You need ${numOfP} posts, ${numOfC} bags of concrete, ${numOfSt} 2x4s, ${numOfB} boards, ${numOfSSc} short screws and, ${numOfLSc} long screws.`;
-
-  console.log(waste);
-  console.log(wasteArray);
 }
