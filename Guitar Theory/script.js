d81ee77a087ac479a45bff48c59907d8e1eb0ed3
CalvinGaiennie <calvingaiennie@gmail.com>
2024-08-14 14:52:15 -0500

    Added A Page

--- Guitar Theory/script.js
+++ Guitar Theory/script.js
@@ -6,3 +6,9 @@ btnUnhide.addEventListener("click", function (event) {
   event.preventDefault();
   image.style.opacity = 100;
 });
+
+// Add a mouseover event listener
+// button.addEventListener("mouseover", () => {
+//   // Change the button's background color
+//   button.style.backgroundColor = "blue";
+// });
