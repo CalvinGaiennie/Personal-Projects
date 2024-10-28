45c0f1056072d35f4733ed75e45c6b38922ac3cb
CalvinGaiennie <calvingaiennie@gmail.com>
2024-10-28 12:55:22 -0500

    Started Working on Adding a Tuner

--- Random Test/script.js
+++ Random Test/script.js
@@ -1,11 +1,1 @@
 "use strict";
-const input = document.getElementById("Input");
-const button = document.getElementById("button");
-const output = document.getElementById("output");
-
-button.addEventListener("click", function () {
-  const inputV = input.value;
-  const inputVNumber = Number(inputV);
-  const newNumber = inputVNumber + 7;
-  output.innerHTML = newNumber;
-});
