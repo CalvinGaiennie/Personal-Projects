c3adfa9649aad9fd5858b15027589e6f299651ed
CalvinGaiennie <calvingaiennie@gmail.com>
2024-12-10 23:40:01 -0600

    explaining JS HTML CSS to Ann

--- Random Test/script.js
+++ Random Test/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const button = document.getElementById("button");
+
+const output = document.getElementById("output");
+
+button.addEventListener("click", function () {
+  const input = document.getElementById("input").value;
+  const doubleInput = input + input;
+  output.innerHTML = doubleInput;
+});
+
+header;
+
+header;
